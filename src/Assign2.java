fecb7de5a9a519a33873b6d0c7b1e66b6ac34c3e
Jonathan Yee <jonathan.yee@ucalgary.ca>
2018-02-02 17:47:51 -0700

    Create LinkedList, Node classes

--- src/Assign2.java
+++ src/Assign2.java
@@ -1,12 +1,14 @@
 /**
  * 
- * @author Jonathan
+ * @author Jonathan Yee
  *
  */
 public class Assign2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<args.length;i++) {
+			System.out.println(args[i]);
+		}
 
 	}
 
---
+++ src/Node.java
@@ -0,0 +1,22 @@
+
+public class Node<T> {
+	Integer keyM;
+	T itemM;
+	Node<T> nextM;
+	
+	public Node()
+	{
+		keyM = null;
+		itemM=  null; 
+		nextM  = null;
+	
+	}
+	public Node(T itemA, Integer keyA, Node<T> nextA)
+	
+	{
+		itemM = itemA ;
+		keyM = keyA;
+		nextM = nextA;
+	}
+
+}
