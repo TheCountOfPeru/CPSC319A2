1a95dbc636ec16a3d4a4d6a6150b67d2f6a108f7
Jonathan Yee <jonathan.yee@cs.ucalgary.ca>
2018-02-07 16:32:13 -0700

    Remove unused functions

--- src/Assign2.java
+++ src/Assign2.java
@@ -23,14 +23,6 @@ public class Assign2 {
 	    }
 	}
 	/*
-	 * Adapted from Data Structures and Algorithms 2nd Edition, Drozdek p.474
-	 */
-	public static void swap(int[] a, int e1, int e2) {
-		int tmp = a[e1]; 
-		a[e1] = a[e2]; 
-		a[e2] = tmp;
-		}
-	/*
 	 * Adapted from Data Structures and Algorithms 2nd Edition, Drozdek p.471
 	 */
 	public static void insertionsortChars(char[] data) {
@@ -45,45 +37,7 @@ public class Assign2 {
 			data[j] = tmp;
 			i++;
 		}//end of outer loop
-	}/*
-	 * Adapted from Data Structures and Algorithms 2nd Edition, Drozdek p.489
-	 */
-	public static void quicksort(int[] data, int first, int last) {
-		int lower = first + 1, upper = last;
-		swap(data,first,(first+last)/2);
-		int bound = data[first];
-		while (lower <= upper) {
-			while (bound > data[lower]) {
-				lower++;
-		}
-		while (bound < data[upper]) 
-			upper--;
-		if (lower < upper)
-			swap(data,lower++,upper--);
-		else lower++;
-		}
-		swap(data,upper,first);
-		if (first < upper-1)
-		quicksort(data,first,upper-1);
-		if (upper+1 < last)
-		quicksort(data,upper+1,last);
-		}
-	/*
-	 * Adapted from Data Structures and Algorithms 2nd Edition, Drozdek p.489
-	 */
-	public static void quicksort(int[] data) {
-		if (data.length < 2)
-			return;
-		int max = 0;
-		// find the largest element and put it at the end of data;
-		for (int i = 1; i < data.length; i++) {
-			if (data[max] < data[i]){
-				max = i;
-			}
-		}
-		swap(data,data.length-1,max); 		// largest el is now in its
-		quicksort(data,0,data.length-2); 	// final position;
-		}
+	}
 	/**
 	 * Compares two strings to see if they are anagrams of each other.
 	 * @param x The first string
