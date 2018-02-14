419b70504385339ff99692f10ee04ae84cd7f940
Jonathan Yee <jonathan.yee@ucalgary.ca>
2018-02-13 22:15:12 -0700

    Added comments to insertionsort for linkedlists

--- src/myLinkedList.java
+++ src/myLinkedList.java
@@ -68,9 +68,9 @@ public class myLinkedList {
 		if (getHead() == null || getHead().getNext() == null)
 			return;
 		
-		//We will allocate a new linkedlist and add to it from the old linkedlist except it will sort the items
-		Node newhead = new Node(getHead().getItem());
-		Node pointer = getHead().getNext();
+		//We will allocate a new linkedlist and add to it from the old linkedlist except it the items will be sorted
+		Node newhead = new Node(getHead().getItem()); //Start of new linked list uses old head
+		Node pointer = getHead().getNext();			  //Node to look at the next node. Needed to traverse the old linked list
 
 		// loop through each element in the list until you get a null node
 		while (pointer != null) {
@@ -79,12 +79,12 @@ public class myLinkedList {
 			Node innerPointer = newhead;
 			Node next = pointer.getNext();
 			
-			if (pointer.getItem().compareTo(newhead.getItem()) <= 0) {
-				Node oldhead = newhead;
+			if (pointer.getItem().compareTo(newhead.getItem()) <= 0) { //If in the old linked list the pointer node has a item less than or equal to
+				Node oldhead = newhead;								   //put the pointer item in the linked list as the new head				
 				newhead = pointer;
 				newhead.setNext(oldhead);
 			}
-			else {
+			else {	//Here we need to find the exact spot the item pointer holds fits between two nodes in the new linked list.
 				while (innerPointer.getNext() != null) {
 					if (pointer.getItem().compareTo(innerPointer.getItem()) > 0 && pointer.getItem().compareTo(innerPointer.getNext().getItem()) <= 0) {
 						Node oldNext = innerPointer.getNext();
@@ -93,7 +93,7 @@ public class myLinkedList {
 					}
 					innerPointer = innerPointer.getNext();
 				}
-				
+					//When the item from pointer is larger than the tail item of the new linked list
 					if(innerPointer.getNext() == null && pointer.getItem().compareTo(innerPointer.getItem()) > 0) {
 						innerPointer.setNext(pointer);
 						pointer.setNext(null);
